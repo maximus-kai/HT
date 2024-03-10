f21189c1727f23d7b70362e3c07b5ffcd279dcae
Maximus <tomiwadairo@gmail.com>
2024-03-11 00:07:38 +0100

    trying out on vs code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+// import Button from '@mui/material/Button';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Button variant="contained">Hello world</Button>
     <App />
   </React.StrictMode>
 );
